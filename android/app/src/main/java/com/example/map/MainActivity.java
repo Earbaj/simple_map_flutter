872f8032fb1a2a1391f983ed0515c1a5a668b322
Earbaj <earbajsaria3@gmail.com>
2021-07-05 21:54:38 +0600

    Added map dependancy and flutter map package

---
+++ android/app/src/main/java/com/example/map/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.map;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        //body: MyApp(),
+      ),
+    )
+  );
+}
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   late GoogleMapController map_controller;
+//   final LatLng _center = const LatLng(23.810332, 90.4125181);
+//
+//   void onMapController(GoogleMapController controller){
+//     map_controller = controller;
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return GoogleMap(
+//          onMapCreated: onMapController,
+//         initialCameraPosition: CameraPosition(target: _center, zoom: 11.0)
+//     );
+//   }
+// }
